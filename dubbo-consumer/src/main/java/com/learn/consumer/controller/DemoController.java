88128e2aaa2d7a38d75aa1905dd6de8343239c99
宋刚 <song_git@foxmail.com>
2019-05-09 13:49:33 +0800

    dubbo  add

--- dubbo-consumer/src/main/java/com/learn/consumer/controller/DemoController.java
+++ dubbo-consumer/src/main/java/com/learn/consumer/controller/DemoController.java
@@ -2,12 +2,12 @@ package com.learn.consumer.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.learn.api.demoService.DemoService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("demo")
 public class DemoController {
 
@@ -16,11 +16,11 @@ public class DemoController {
 
 
     @RequestMapping(value = "hello",method = RequestMethod.POST)
-    public void demoController(@RequestParam(value = "name", required = true)String name){
+    public Object demoController(@RequestParam(value = "name", required = true)String name){
         String hello = demoService.sayHello(name);
         System.out.println(hello);
 
-
+        return hello;
     }
 
 }
--- dubbo-provider/src/main/java/com/learn/provider/impl/DemoServiceImpl.java
+++ dubbo-provider/src/main/java/com/learn/provider/impl/DemoServiceImpl.java
@@ -1,20 +1,20 @@
 package com.learn.provider.impl;
 
 import com.learn.api.demoService.DemoService;
+import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-
+@Component
 @com.alibaba.dubbo.config.annotation.Service(version = "demoService")
 public class DemoServiceImpl implements DemoService {
 
     @Override
     public String sayHello(String name){
         String hello = "hello:" + name  ;
-        try {
-            System.in.read();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        //try {
+        //    System.in.read();
+        //} catch (IOException e) {
+        //    e.printStackTrace();
+        //}
         return hello;
     }
 }
